f53ceb9db6e5fb7b5cab75bc0d47dbf21bdf0f84
divyo-argha <arghastm@gmail.com>
2026-06-13 00:53:23 +0600

    chore: save cryptographic pins for v3.0.8

--- npm/bin/git-user.js
+++ npm/bin/git-user.js
@@ -13,7 +13,28 @@ const REPO = 'divyo-argha/git-user';
 const VERSION = `v${PKG_JSON.version}`;
 
 // --- START PINNED HASHES ---
-const PINNED_HASHES = {};
+const PINNED_HASHES = {
+    "git-user_darwin_arm64.tar.gz": {
+      "archive": "759e6a4137bc9caed58b393f751fa4c670a3dea98ef072a6551f38d41a9fc9ce",
+      "binary": "9b39841ca909f8dfcffff9a856771a65a8615ffb5e872e4c1108f0fa2e0d1752"
+    },
+    "git-user_darwin_x86_64.tar.gz": {
+      "archive": "eda5a46ea9f440174408d33659de38b7623da28b94ee447a3dc7fa509ffced83",
+      "binary": "efd1e62a40659f5e9132448ae5d1c5a5f23069a5277db73641de726cd28c55f1"
+    },
+    "git-user_linux_arm64.tar.gz": {
+      "archive": "3e311264bedf0672eee3625396d2e42d1c72d2a263a56fe2a7de9e5c54a2e2d3",
+      "binary": "f177a44c4c7d0a79840a404b669ce924c0117d58db6d407880007684efea64a2"
+    },
+    "git-user_linux_x86_64.tar.gz": {
+      "archive": "aab7e800a2b49a265409e0fbc3c64d4282b334bca222ea975532bd92dc7869dc",
+      "binary": "31172516986f3020bda2d5186ef7034919c09690399f849dcc980b542ed060f8"
+    },
+    "git-user_windows_x86_64.tar.gz": {
+      "archive": "eee27a75cfa131053b0901d8432216881aae25fda4586258b9b357ffa3558455",
+      "binary": "36e2ebc6272fe828b9b6a49d2623a049d001e826cca2c6ad428f178e9ca09403"
+    }
+  };
 // --- END PINNED HASHES ---
 
 const platform = os.platform();
