fadc19f57f37c5e77783c0fb06bb4adf96e5d914
divyo-argha <arghastm@gmail.com>
2026-07-26 20:00:25 +0600

    feat: expand command list, improve headless keyring error handling

--- internal/keyring/keyring.go
+++ internal/keyring/keyring.go
@@ -1,6 +1,9 @@
 package keyring
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/zalando/go-keyring"
 )
 
@@ -12,12 +15,29 @@ var (
 	KeyringDelete = keyring.Delete
 )
 
+func formatHeadlessError(err error) error {
+	if err == nil {
+		return nil
+	}
+	
+	errStr := strings.ToLower(err.Error())
+	if strings.Contains(errStr, "dbus") || strings.Contains(errStr, "secret") || strings.Contains(errStr, "unsupported") {
+		return fmt.Errorf("%w (headless/SSH session? GUI keychain may be unavailable)", err)
+	}
+	return err
+}
+
 func SetKeychainPassphrase(profileName, passphrase string) error {
-	return KeyringSet(keychainService, profileName, passphrase)
+	err := KeyringSet(keychainService, profileName, passphrase)
+	return formatHeadlessError(err)
 }
 
 func GetKeychainPassphrase(profileName string) (string, error) {
-	return KeyringGet(keychainService, profileName)
+	val, err := KeyringGet(keychainService, profileName)
+	if err == keyring.ErrNotFound {
+		return val, err
+	}
+	return val, formatHeadlessError(err)
 }
 
 func DeleteKeychainPassphrase(profileName string) error {
@@ -25,5 +45,5 @@ func DeleteKeychainPassphrase(profileName string) error {
 	if err == keyring.ErrNotFound {
 		return nil
 	}
-	return err
+	return formatHeadlessError(err)
 }
