eabe3bc4b497797fdbbd3e9207c5582671c558aa
divyo-argha <arghastm@gmail.com>
2026-08-11 17:50:16 +0600

    chore: update version to v4.7.3

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 // Version is the current fallback version of git-user.
 // It can be overridden at build time using:
 //	-ldflags="-X github.com/divyo-argha/git-user/internal/version.Version=vX.Y.Z"
-var Version = "v4.7.2"
+var Version = "v4.7.3"
 
 var BuildVersion = ""
 
